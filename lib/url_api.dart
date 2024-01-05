c27034455d82d8c99eabdc850ffd43b05b1c255a
nhatpan <nhatkb2001@gmail.com>
2024-01-05 11:10:34 +0700

    create url-api

---
+++ lib/url_api.dart
@@ -0,0 +1,7 @@
+class UrlApi {
+  static const String userApi = 'api/User/';
+  static const String loginUser = '$userApi/login';
+  static const String registerUser = '$userApi/register';
+  static const String otp = '$userApi/otp';
+  static const String verifyOtp = '$userApi/verify';
+}
