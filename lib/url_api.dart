c426fbb109c03427d6a0d910959ab1ae731ece03
nhatpan <nhatkb2001@gmail.com>
2024-01-05 13:57:05 +0700

    fix url

--- lib/url_api.dart
+++ lib/url_api.dart
@@ -1,5 +1,5 @@
 class UrlAPI {
-  static const String userApi = 'api/User/';
+  static const String userApi = 'api/User';
   static const String loginUser = '$userApi/login';
   static const String registerUser = '$userApi/register';
   static const String otp = '$userApi/otp';
